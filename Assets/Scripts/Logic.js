2040513db3c82393dd7a830ca71a44216472ff31
David Wright <dontbothertheking@gmail.com>
2017-06-13 15:16:45 -0500

    minor changed and made connect with me links auto hook up with javascript

--- Assets/Scripts/Logic.js
+++ Assets/Scripts/Logic.js
@@ -1,7 +1,35 @@
+// Automatically image sources
 $(document).ready(function () {
-    $("*").css({
-        "border-color": "black",
-        "border-weight": "1px",
-        "border-style": "solid"
-    });
+    LoadContactRefs();
 });
+
+/**
+ * Loads the nav bar references
+
+
+/**
+ * Loads the contact image references
+ */
+function LoadContactRefs() {
+    var imageObjects = $("img").get();
+
+    for (var i = 0; i < imageObjects.length; i++) {
+        var img = imageObjects[i];
+        var imageSource = $(img).attr('src');
+        var sourceEnd = imageSource.substring(14, imageSource.length)
+
+        switch (sourceEnd) {
+            case "github.png":
+                $(img).parent().attr("href", "https://github.com/captainsalt")
+                break;
+            case "linkedin.png":
+                $(img).parent().attr("href", "https://www.linkedin.com/in/david-wright-659006144/")
+                break;
+            case "stack.png":
+                $(img).parent().attr("href", "https://stackoverflow.com/users/7107832/captainsalt")
+                break;
+            default:
+                break;
+        }
+    }
+}
