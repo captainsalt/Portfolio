5e34d5669816bd031368a3316e5a1503dba8f520
David Wright <dontbothertheking@gmail.com>
2017-06-13 19:07:51 -0500

    hardcoded links because the auto link only worked for main page for some reason

--- Assets/Scripts/Logic.js
+++ Assets/Scripts/Logic.js
@@ -1,31 +1,39 @@
 // Automatically image sources
-$(document).ready(function () {
-    LoadContactRefs();
-});
+// $(document).on("pagecontainerload", function () {
+//     LoadContactRefs();
+// });
+
+// window.onload = function () {
+//     LoadContactRefs();
+// }
 
 /**
  * Loads the contact image references
  */
-function LoadContactRefs() {
-    var imageObjects = $("img").get();
+// function LoadContactRefs() {
+//     var imageObjects = $("img").get();
 
-    for (var i = 0; i < imageObjects.length; i++) {
-        var img = imageObjects[i];
-        var imageSource = $(img).attr('src');
-        var sourceEnd = imageSource.substring(14, imageSource.length)
+//     for (var i = 0; i < imageObjects.length; i++) {
+//         var img = imageObjects[i];
+//         var imageSource = $(img).attr('src');
+//         var sourceEnd = imageSource.substring(14, imageSource.length)
 
-        switch (sourceEnd) {
-            case "github.png":
-                $(img).parent().attr("href", "https://github.com/captainsalt")
-                break;
-            case "linkedin.png":
-                $(img).parent().attr("href", "https://www.linkedin.com/in/david-wright-659006144/")
-                break;
-            case "stack.png":
-                $(img).parent().attr("href", "https://stackoverflow.com/users/7107832/captainsalt")
-                break;
-            default:
-                break;
-        }
-    }
-}
+//         switch (sourceEnd) {
+//             case "github.png":
+//                 $(img).parent().attr("href", "https://github.com/captainsalt")
+//                 break;
+//             case "linkedin.png":
+//                 $(img).parent().attr("href", "https://www.linkedin.com/in/david-wright-659006144/")
+//                 break;
+//             case "stack.png":
+//                 $(img).parent().attr("href", "https://stackoverflow.com/users/7107832/captainsalt")
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }
+// }
+
+// $("button").mouseover(function () {
+//     $(this).hide();
+// })
